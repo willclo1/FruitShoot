6f3e8311210371677def6ca7663413913d560edd
willclo1 <115596953+willclo1@users.noreply.github.com>
2026-02-05 17:02:11 -0600

    fixed it?

--- frontend/App.js
+++
@@ -1,8 +0,0 @@
-// This is the entry point for the app
-// This file should not really change much
-
-import RootNavigator from "./src/navigation/RootNavigator";
-
-export default function App() {
-  return <RootNavigator />;
-}
