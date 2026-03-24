b7fd1c0c02e17c3ad2282d783c77c9df2c398866
willclo1 <115596953+willclo1@users.noreply.github.com>
2026-03-24 14:31:50 -0500

    updated ecosystem

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -2,16 +2,17 @@ module.exports = {
   apps: [
     {
       name: "fruitshoot-expo",
-      cwd: "./frontend",
+      cwd: ".",
       script: "npx",
-      args: "expo start --tunnel",
+      args: "expo start --tunnel --clear",
       env: {
         CI: "1"
       },
       autorestart: true,
       watch: false,
-      max_restarts: 10,
-      restart_delay: 5000
+      max_restarts: 20,
+      restart_delay: 5000,
+      time: true
     }
   ]
 };
