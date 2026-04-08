fa17ecb9fbe2d59adcd1d572d778b3e0c0391869
willclo1 <115596953+willclo1@users.noreply.github.com>
2026-04-07 22:56:25 -0500

    last change

--- backend/main.py
+++ backend/main.py
@@ -64,17 +64,20 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(title="FruitShoot API", lifespan=lifespan)
 
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "http://localhost:8000",
+        "http://127.0.0.1:8000",
+        "http://134.199.138.23",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 Base.metadata.create_all(bind=engine)
 
 
